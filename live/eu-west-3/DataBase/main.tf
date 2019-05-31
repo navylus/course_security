f3ce08f84a4bbcaff452f732382c28dba815f49a
Navylus <sulivan.gorjao@gmail.com>
2019-05-31 18:49:29 +0200

    update postgres in order to allow minor changes

--- live/eu-west-3/DataBase/main.tf
+++ live/eu-west-3/DataBase/main.tf
@@ -101,7 +101,7 @@ resource "aws_db_instance" "default" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "postgres"
-  engine_version         = "9.6.3"
+  engine_version         = "9.6"
   instance_class         = "db.t2.micro"
   name                   = "${var.nameDB}"
   username               = "${var.userDB}"
